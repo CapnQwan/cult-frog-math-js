bf76a7f6df0753ac7227c03479617889162dac70
CapnQwan <capn.qwan1519@gmail.com>
2026-09-18 16:55:21 +1200

    Fixed the Readonly vectors were not exported

--- src/vector/_types/index.ts
+++ src/vector/_types/index.ts
@@ -1,3 +1,3 @@
-export type { Vec2 } from './vec2.js';
-export type { Vec3 } from './vec3.js';
-export type { Vec4 } from './vec4.js';
+export type { ReadonlyVec2, Vec2 } from './vec2.js';
+export type { ReadonlyVec3, Vec3 } from './vec3.js';
+export type { ReadonlyVec4, Vec4 } from './vec4.js';
